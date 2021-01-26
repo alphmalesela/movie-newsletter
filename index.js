5ec407b97ed7bb8bb90d6d227a3c075c5f757f1f
Malesela Seabi <seabiali@gmail.com>
2021-01-27 00:40:33 +0200

    Start Ui

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-
-var app = express();
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, './views');
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+app.use('/', express.static(publicPath));
 
 app.get('/', function (req, res) {
     res.render('home');
