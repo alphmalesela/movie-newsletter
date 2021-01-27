569cb8e8b3deeeaa5b0723c702435b5011858183
Malesela Seabi <seabiali@gmail.com>
2021-01-27 15:01:55 +0200

    Post user data. Redirect to confirm page.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded({     
+  extended: true
+})); 
+
 const publicPath = path.join(__dirname, './views');
 
 app.engine('handlebars', exphbs());
@@ -13,4 +20,12 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.listen(3000);
+app.post('/', function (req, res) {
+    console.log('req.body:. ');
+    console.log(req.body);
+    res.render('confirmation');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port:3000');
+});
